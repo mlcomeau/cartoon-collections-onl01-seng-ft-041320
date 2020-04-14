ad0af35625097506ea99f3098453d8bb69daacc0
Megan Comeau <mlcomeau99@gmail.com>
2020-04-14 23:24:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
   # Your code here
 end
 
